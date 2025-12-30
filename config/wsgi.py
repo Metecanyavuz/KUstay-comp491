ee7e98f8ec5a621912ee3e0427663c789a2625d7
emrenkaratas19 <ekaratas21@ku.edu.tr>
2025-12-30 23:04:56 +0300

    bug fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+logger = logging.getLogger(__name__)
+try:
+    os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+    os.makedirs(os.path.join(settings.MEDIA_ROOT, "listing_images"), exist_ok=True)
+    logger.info(
+        "MEDIA_ROOT=%s MEDIA_URL=%s SERVE_MEDIA=%s",
+        settings.MEDIA_ROOT,
+        settings.MEDIA_URL,
+        settings.SERVE_MEDIA,
+    )
+except Exception:
+    logger.exception("Failed to initialize media directories")
